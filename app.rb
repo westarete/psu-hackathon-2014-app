cc163d57069262ea80edd264b1f8b429392fd4a1
Scott Woods <scott@westarete.com>
2014-04-04 02:04:39 -0400

    Clarify with comments

--- app.rb
+++ app.rb
@@ -5,8 +5,10 @@ require 'sinatra/reloader' if development?
 require 'geocoder'
 require 'forecast_io'
 
+# Make sure we're using a gem that works on both Mac and Windows.
 MultiJson.use(:json_pure)
 
+# Set our API key for talking to forecast.io
 ForecastIO.api_key = ENV['FORECAST_API_KEY'] or raise "Set the FORECAST_API_KEY environment variable"
 
 helpers do
@@ -23,13 +25,20 @@ helpers do
 end
 
 get '/' do
+  # Find the lat/lon of the given location.
   @location = Geocoder.search(params[:location]).first
-  start_time = Time.now + 1*60*60
-  lunch_time = Time.now + 5*60*60
-  end_time = Time.now + 10*60*60
-  @forecast_start = ForecastIO.forecast(@location.latitude, @location.longitude, :time => start_time.to_i)
-  @forecast_lunch = ForecastIO.forecast(@location.latitude, @location.longitude, :time => lunch_time.to_i)
-  @forecast_end   = ForecastIO.forecast(@location.latitude, @location.longitude, :time => end_time.to_i)
+
+  # Figure out what times we'll use to fetch forecasts.
+  start_time = Time.now.to_i + 1*60*60
+  lunch_time = Time.now.to_i + 5*60*60
+  end_time = Time.now.to_i + 10*60*60
+
+  # Get the forecasts.
+  @forecast_start = ForecastIO.forecast(@location.latitude, @location.longitude, :time => start_time)
+  @forecast_lunch = ForecastIO.forecast(@location.latitude, @location.longitude, :time => lunch_time)
+  @forecast_end   = ForecastIO.forecast(@location.latitude, @location.longitude, :time => end_time)
+
+  # Render the view.
   erb :index
 end
 
