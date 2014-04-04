d617f275482bcb3f1fdf3176b7e554a5a31894f7
Scott Woods <scott@westarete.com>
2014-04-04 01:16:15 -0400

    Prove that we can geocode addresses

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'geocoder'
 
 get '/' do
+  @location = Geocoder.search(params[:location]).first 
   erb :index
 end
