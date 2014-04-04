702f0f31d76a88161c9500b482a6a1c62976a478
Scott Woods <scott@westarete.com>
2014-04-04 01:35:52 -0400

    Fetch and display current conditions

--- app.rb
+++ app.rb
@@ -3,8 +3,14 @@ require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'geocoder'
+require 'forecast_io'
+
+MultiJson.use(:json_pure)
+
+ForecastIO.api_key = ENV['FORECAST_API_KEY'] or raise "Set the FORECAST_API_KEY environment variable"
 
 get '/' do
-  @location = Geocoder.search(params[:location]).first 
+  @location = Geocoder.search(params[:location]).first
+  @forecast = ForecastIO.forecast(@location.latitude, @location.longitude)
   erb :index
 end
