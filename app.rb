26eaf3cd6a59108acad731e003466789f5a9f01f
Scott Woods <scott@westarete.com>
2014-04-04 01:59:39 -0400

    Tell people what to wear

--- app.rb
+++ app.rb
@@ -9,6 +9,19 @@ MultiJson.use(:json_pure)
 
 ForecastIO.api_key = ENV['FORECAST_API_KEY'] or raise "Set the FORECAST_API_KEY environment variable"
 
+helpers do
+  # Given a forecast, return a string to represent the type of attire that should be worn.
+  def attire(forecast)
+    if forecast.currently.temperature < 40
+      "hat"
+    elsif forecast.currently.precipProbability >= 0.5
+      "raincoat"
+    else
+      "tshirt"
+    end
+  end
+end
+
 get '/' do
   @location = Geocoder.search(params[:location]).first
   start_time = Time.now + 1*60*60
@@ -19,3 +32,4 @@ get '/' do
   @forecast_end   = ForecastIO.forecast(@location.latitude, @location.longitude, :time => end_time.to_i)
   erb :index
 end
+
