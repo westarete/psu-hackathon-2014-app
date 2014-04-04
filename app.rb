3efc71e67a2322ded5dd483e18ca7322f73f7218
Scott Woods <scott@westarete.com>
2014-04-04 01:46:11 -0400

    Provide forecasts for the whole day

--- app.rb
+++ app.rb
@@ -11,6 +11,11 @@ ForecastIO.api_key = ENV['FORECAST_API_KEY'] or raise "Set the FORECAST_API_KEY 
 
 get '/' do
   @location = Geocoder.search(params[:location]).first
-  @forecast = ForecastIO.forecast(@location.latitude, @location.longitude)
+  start_time = Time.now + 1*60*60
+  lunch_time = Time.now + 5*60*60
+  end_time = Time.now + 10*60*60
+  @forecast_start = ForecastIO.forecast(@location.latitude, @location.longitude, :time => start_time.to_i)
+  @forecast_lunch = ForecastIO.forecast(@location.latitude, @location.longitude, :time => lunch_time.to_i)
+  @forecast_end   = ForecastIO.forecast(@location.latitude, @location.longitude, :time => end_time.to_i)
   erb :index
 end
